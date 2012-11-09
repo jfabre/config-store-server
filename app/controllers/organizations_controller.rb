270057e117c235aaf0ee3d7074530106f3f24404
Jeremy Fabre <jeremy.fabre@hotmail.com>
2012-11-09 17:32:21 -0500

    Fix issue with org show on json

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -18,6 +18,7 @@ class OrganizationsController < ApplicationController
 
     respond_to do |format|
       format.html # show.html.erb
+      format.json { render json: @organization }
       format.xml { render xml: @organization }
     end
   end
